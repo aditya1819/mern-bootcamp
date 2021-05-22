63fa6eb6459112a8d38bf90618fdcf1bde6734f9
aditya1819 <shahare.aditya@gmail.com>
2021-05-22 13:19:37 +0530

    authentication

--- projbackend/routes/auth.js
+++ projbackend/routes/auth.js
@@ -1,14 +1,31 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
-const { signout, signup } = require('../controllers/auth');
-const { check, validationResult } = require('express-validator');
+const { signout, signup, signin, isSignedIn } = require("../controllers/auth");
+const { check, validationResult } = require("express-validator");
 
-
-router.post("/signup",[
-    check("name","Name must be atleast 3 char").isLength({ min: 3}),
+router.post(
+  "/signup",
+  [
+    check("name", "Name must be atleast 3 char").isLength({ min: 3 }),
     check("email", "Please enter Valid email").isEmail(),
-    check("password", "Password must be atleast 3 char").isLength({ min: 3})
-], signup);
+    check("password", "Password must be atleast 3 char").isLength({ min: 3 }),
+  ],
+  signup
+);
+
+router.post(
+  "/signin",
+  [
+    check("email", "Email id Required !").isEmail(),
+    check("password", "Password Required !").isLength({ min: 3 }),
+  ],
+  signin
+);
+
 router.get("/signout", signout);
+
+router.get("/testroute", isSignedIn, (req, res) => {
+    res.json(req.auth);
+});
 
 module.exports = router;
